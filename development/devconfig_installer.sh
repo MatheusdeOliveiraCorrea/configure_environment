5dd4e28b4adcaaba9d6c2258e66e1095fb75c9d4
Matheus de Oliveira <matheus82pessoal@protonmail.com>
2024-02-22 20:29:03 -0500

    install vscode and .net 8

---
+++ development/devconfig_installer.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+# install vscode
+# https://code.visualstudio.com/docs/setup/linux
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+dnf check-update
+sudo dnf install code
+
+# install .net 8 sdk 
+sudo dnf install dotnet-sdk-8.0
